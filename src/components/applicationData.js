7dd2cd05dc64f3c4ba15d6632c8a894a81c1d95c
Lexy Kramer <lexy.kramer@gmail.com>
2021-04-12 14:13:45 -0400

    Added to Lucky's Pharmacy description

--- src/components/applicationData.js
+++ src/components/applicationData.js
@@ -16,7 +16,7 @@ const applications = [
     image:
       "https://res.cloudinary.com/dli6sknqy/image/upload/v1618204055/Screen_Shot_2021-04-11_at_10.56.28_AM_pyypyo.png",
     description:
-      "Entry for a hackathon with the prompt being: identify a problem from a decade of your choosing and solve it with an app. My team chose prohibition in the 1920's. We made an alcohol ordering service disguised as a pharmacy site.",
+      "Entry for a hackathon with the prompt: identify a problem from a decade of your choosing and try to solve it. My team chose prohibition in the 1920's. We made an alcohol ordering service disguised as a pharmacy site. Our goal was to give the service a speakeasy feel.",
     website: null,
     repo: "https://github.com/lexykio/the-pharmacy",
     alt: "the pharmacy preview",
